1811c942a778001df184e338660189043ce4d2b4
Juan Pablo Ballistreri <jpballistreri@gmail.com>
2022-06-06 01:35:05 -0300

    commit 0

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import pelota from "./pelota.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={pelota} className="App-logo" alt="logo" />
+        <p Style={"font-size: 30px"}>mercado fulbo</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
