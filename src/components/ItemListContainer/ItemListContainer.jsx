de28b4f93c53f56df996b80ad31f86e2a21337b0
Juan Pablo Ballistreri <jpballistreri@gmail.com>
2022-06-23 10:54:22 -0300

    Se deja estructura pedida para ItemListContainer

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,37 @@
-import React from "react";
 import { Container } from "@mui/material";
 import ItemList from "./ItemList";
+import React, { useEffect, useState } from "react";
+import productosJson from "../../items.json";
+import { Grid } from "@mui/material";
+import Pelota from "../Pelota/Pelota";
 
 function ItemListContainer() {
+  const [productos, setProductos] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    let itemsLoad;
+    itemsLoad = new Promise((res, rej) => {
+      setTimeout(() => {
+        if (productosJson.length > 0) {
+          res(productosJson);
+        } else {
+          rej("No hay productos");
+        }
+      }, 2000);
+    });
+
+    itemsLoad
+      .then((res) => {
+        setProductos(res);
+      })
+      .catch((err) => setError(true))
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <Container
       sx={{
@@ -12,7 +41,13 @@ function ItemListContainer() {
         paddingTop: "30px",
       }}
     >
-      <ItemList />
+      {loading && (
+        <Grid item>
+          <Pelota classPelota="PelotaCarga" />
+        </Grid>
+      )}
+      {error && <Grid item>"ERROR: Intenta ingresar en unos minutos."</Grid>}
+      {productos && <ItemList productos={productos} />}
     </Container>
   );
 }
